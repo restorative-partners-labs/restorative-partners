fdc9531f9255182471864dbb0c115bff6d91eaca
ladellerby <ladellerby@gmail.com>
2024-04-07 23:05:48 -0700

    - update nextjs options for /admin sanity studio page.

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    trailingSlash: true,
+  trailingSlash: true,
 
-    images: {
-        unoptimized: true
-    },
-    
-    output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
